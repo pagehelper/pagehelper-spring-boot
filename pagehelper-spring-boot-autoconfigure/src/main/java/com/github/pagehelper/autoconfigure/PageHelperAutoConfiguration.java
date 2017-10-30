6dadeed2e83e74b9295cb078f8475db8423ca400
trang <donghao.l@hotmail.com>
2017-10-30 13:24:59 +0800

    * 升级 mybatis 到 3.4.5 * 升级 springboot 到 1.5.8.RELEASE * 修复 pagehelper-spring-boot-samples 版本号不正确的问题 * 修改 PageInterceptor 的注册方式为 ConfigurationCustomizer

--- pagehelper-spring-boot-autoconfigure/src/main/java/com/github/pagehelper/autoconfigure/PageHelperAutoConfiguration.java
+++ pagehelper-spring-boot-autoconfigure/src/main/java/com/github/pagehelper/autoconfigure/PageHelperAutoConfiguration.java
@@ -25,18 +25,13 @@
 package com.github.pagehelper.autoconfigure;
 
 import com.github.pagehelper.PageInterceptor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -45,16 +40,8 @@ import java.util.Properties;
  * @author liuzh
  */
 @Configuration
-@ConditionalOnBean(SqlSessionFactory.class)
 @EnableConfigurationProperties(PageHelperProperties.class)
-@AutoConfigureAfter(MybatisAutoConfiguration.class)
 public class PageHelperAutoConfiguration {
-
-    @Autowired
-    private List<SqlSessionFactory> sqlSessionFactoryList;
-
-    @Autowired
-    private PageHelperProperties properties;
 
     /**
      * 接受分页插件额外的属性
@@ -67,18 +54,24 @@ public class PageHelperAutoConfiguration {
         return new Properties();
     }
 
-    @PostConstruct
-    public void addPageInterceptor() {
-        PageInterceptor interceptor = new PageInterceptor();
-        Properties properties = new Properties();
-        //先把一般方式配置的属性放进去
-        properties.putAll(pageHelperProperties());
-        //在把特殊配置放进去，由于close-conn 利用上面方式时，属性名就是 close-conn 而不是 closeConn，所以需要额外的一步
-        properties.putAll(this.properties.getProperties());
-        interceptor.setProperties(properties);
-        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
-            sqlSessionFactory.getConfiguration().addInterceptor(interceptor);
-        }
+    @Bean
+    public ConfigurationCustomizer pageHelperConfigurationCustomizer() {
+        return new ConfigurationCustomizer() {
+            @Autowired
+            private PageHelperProperties properties;
+
+            @Override
+            public void customize(org.apache.ibatis.session.Configuration configuration) {
+                PageInterceptor interceptor = new PageInterceptor();
+                Properties properties = new Properties();
+                //先把一般方式配置的属性放进去
+                properties.putAll(pageHelperProperties());
+                //在把特殊配置放进去，由于close-conn 利用上面方式时，属性名就是 close-conn 而不是 closeConn，所以需要额外的一步
+                properties.putAll(this.properties.getProperties());
+                interceptor.setProperties(properties);
+                configuration.addInterceptor(interceptor);
+            }
+        };
     }
 
 }
